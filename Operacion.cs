be6bb4b52cfab8a79e186b1d18c95155922db51a
marisolmaldonado <magaly.mary.98@gmail.com>
2021-01-09 14:21:24 -0500

    Add files via upload

---
+++ Operacion.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LSP
+{
+    public abstract class Operacion
+    {
+        public double OperadorUno { get; set; }
+        public double OperadorDos { get; set; }
+        public abstract double Calcular();
+    }
+
+    public class OperacionSumar : Operacion
+    {
+        public override double Calcular()
+        {
+            double result = this.OperadorDos + this.OperadorDos;
+            return result;
+        }
+    }
+
+    public class OperacionRestar : Operacion
+    {
+        public override double Calcular()
+        {
+            double result = this.OperadorDos - this.OperadorDos;
+            return result;
+        }
+    }
+
+    public class OperacionMultiplicar : Operacion
+    {
+        public override double Calcular()
+        {
+            double result = this.OperadorDos * this.OperadorDos;
+            return result;
+        }
+    }
+
+    public class OperacionDividir : Operacion
+    {
+        public override double Calcular()
+        {
+            double result = this.OperadorDos / this.OperadorDos;
+            return result;
+        }
+    }
+
+    public class Calculadora
+    {
+        public void Calcular(Operacion op)
+        {
+            double result = op.Calcular();
+            Console.WriteLine("Resultado=" + result);
+        }
+    }
+
+}
---
+++ Program.cs
@@ -0,0 +1,44 @@
+﻿namespace LSP
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Calculadora calc = new Calculadora();
+
+            OperacionSumar suma = new OperacionSumar
+            {
+                OperadorUno = 10,
+                OperadorDos = 4
+            };
+
+            calc.Calcular(suma);
+
+
+            OperacionRestar resta = new OperacionRestar
+            {
+                OperadorUno = 10,
+                OperadorDos = 4
+            };
+
+            calc.Calcular(resta);
+
+            OperacionMultiplicar multiplicacion = new OperacionMultiplicar
+            {
+                OperadorUno = 6,
+                OperadorDos = 5
+            };
+
+            calc.Calcular(multiplicacion);
+
+
+            OperacionDividir division = new OperacionDividir
+            {
+                OperadorUno = 6,
+                OperadorDos = 5
+            };
+
+            calc.Calcular(division);
+        }
+    }
+}
